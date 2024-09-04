6c118908d386ba81497e22e1d0d458631f33a12f
Blarissa <larissabrasil009@gmail.com>
2024-09-03 22:10:53 -0300

    chore: Update ProjetoRepository with CRUD operations

--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 namespace Exo.WebApi.Repositories
 {
     public class ProjetoRepository
@@ -16,6 +17,40 @@ namespace Exo.WebApi.Repositories
         public List<Projeto> Listar()
         {
             return _context.Projetos.ToList();
+        }
+
+        public void Cadastrar(Projeto projeto)
+        {
+            _context.Projetos.Add(projeto);
+            _context.SaveChanges();
+        }
+
+        public Projeto BuscarPorId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+
+        public void Atualizar(int id, Projeto projeto)
+        {
+            var projetoBuscado = BuscarPorId(id);
+
+            if (projetoBuscado != null)
+            {
+                projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+                projetoBuscado.Area = projeto.Area;
+                projetoBuscado.Status = projeto.Status;
+            }
+
+            _context.Projetos.Update(projeto);
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var projetoBuscado = BuscarPorId(id);
+            
+            _context.Projetos.Remove(projetoBuscado);
+            _context.SaveChanges();
         }
     }
 }
