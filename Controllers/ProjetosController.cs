37400afc8819ed7ac1ebeb835fa87d5bd77b63b9
Blarissa <larissabrasil009@gmail.com>
2024-09-03 22:40:13 -0300

    chore: Refactor ProjetosController and add CRUD operations

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -3,6 +3,7 @@ using Exo.WebApi.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+
 namespace Exo.WebApi.Controllers
 {
     [Route("api/[controller]")]
@@ -10,14 +11,64 @@ namespace Exo.WebApi.Controllers
     public class ProjetosController : ControllerBase
     {
         private readonly ProjetoRepository _projetoRepository;
+        
         public ProjetosController(ProjetoRepository projetoRepository)
         {
             _projetoRepository = projetoRepository;
         }
+
         [HttpGet]
         public IActionResult Listar()
         {
             return Ok(_projetoRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Projeto projeto)
+        {
+            _projetoRepository.Cadastrar(projeto);
+            return StatusCode(201);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            var projetoBuscado = _projetoRepository.BuscarPorId(id);
+
+            if (projetoBuscado == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(projetoBuscado);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Projeto projeto)
+        {
+            var projetoBuscado = _projetoRepository.BuscarPorId(id);
+
+            if (projetoBuscado == null)
+            {
+                return NotFound();
+            }
+
+            _projetoRepository.Atualizar(id, projeto);
+            return StatusCode(204);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            var projetoBuscado = _projetoRepository.BuscarPorId(id);
+
+            try 
+            {
+                _projetoRepository.Deletar(id);
+                return StatusCode(204);
+            } catch (Exception e) {
+                return BadRequest(e);
+            }            
         }
     }
 }
